6f413a4a6a91ac90a30a3fe358ae04af45cb76b3
Stelazu <stevenlazuardy2@gmail.com>
2024-05-19 22:36:48 +0700

    Dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard - Laravel Admin Panel With Login And Registration')
+
+@section('contents')
+    <div class="row">
+        Dashboard
+    </div>
+@endsection
